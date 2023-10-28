1a864da6ffa073dde6ef767ccb05adca1627805a
harsh123-baba <harshitgoyal213@gmail.com>
2023-10-28 14:54:50 +0530

    next01

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,7 +1,8 @@
 import getUser from "@/lib/getUser"
 import getUserPosts from "@/lib/getUserPosts"
 import { Suspense } from "react";
-
+import UserPosts from "./components/UserPosts";
+import type { Metadata } from "next";
 type Params = {
     params : {
         userId :string 
@@ -9,17 +10,28 @@ type Params = {
 }
 
 
-export default function UserPage({params : {userId}} : Params) {
+export async function genrateMetadata({params : {userId}} : Params ) : Promise<Metadata>{
+  const userData : Promise<User> =getUser(userId);
+  const user : User = await userData;
+
+  return {
+    title : user.name,
+    description : `This is the page of ${user.name}`
+  }
+}
+
+export default async function UserPage({params : {userId}} : Params) {
   const userData : Promise<User> = getUser(userId)
   const userPostsData : Promise<Post[]> = getUserPosts(userId);
 
-  const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  const user = await userData;
   return (
     <div>
       <h2>{user.name}</h2>
       <br/>
       <Suspense fallback={<h2>Loading....</h2>}>
-        <UserPosts post = {userPosts} />
+        {/* promise is just name dont get confuse we named it here promise bqs we are getting returned promise from userPosts data */}
+        <UserPosts promise = {userPostsData} />
       </Suspense>
 
     </div>
